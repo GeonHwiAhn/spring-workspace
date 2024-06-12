5f1b06e111858b98f657dc84f5e363cacfa0e1ef
GeonHwiAhn <169108642+GeonHwiAhn@users.noreply.github.com>
2024-06-12 17:39:09 +0900

    RequsetParam ModelAttribute 어노테이션 파라미터

--- demoProject1/src/main/java/com/example/demo/controller/CafeController.java
+++ demoProject1/src/main/java/com/example/demo/controller/CafeController.java
@@ -5,13 +5,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.model.dto.CafeDTO;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping
 @Slf4j
 public class CafeController {
-	
+	/*
 	// @어노테이션 PostMapping(만남의주소)
 	// 메서드 cafeMainComment
 	@PostMapping("/cafe/comment")
@@ -29,5 +31,21 @@ public class CafeController {
 		//log.info로 들어왔는지 확인
 		//log.debug로 cafeCommentName cafeCommentOpinion 넣기
 	//return redirect cafe_index로 돌아가기
+	*/
 	
+	
+	//CafeDTO cafe_board
+	
+	//-> @PostMapping("cafe_index") ModelAttribute 받아오기
+	@PostMapping("/cafe/comment")
+	public String cafeIndexComment(CafeDTO cafeDTo) {
+		CafeDTO cd = new CafeDTO();
+		cd.getCafeCommentName();
+		cd.getCafeCommentOpinion();
+		
+		cd.setCafeCommentName("넣어줄 값 = 나중에 db연결해서 전달해줄 값 넣어줄 것");
+		cd.setCafeCommentOpinion("넣어줄 값 = 나중에 db연결해서 전달해줄 값 넣어줄 것");
+		
+		return "redirect:/cafe/cafe_index";
+	}
 }
---
+++ demoProject1/src/main/java/com/example/demo/model/dto/CafeDTO.java
@@ -0,0 +1,14 @@
+package com.example.demo.model.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class CafeDTO {
+	private String cafeCommentName;
+	private String cafeCommentOpinion;
+}
