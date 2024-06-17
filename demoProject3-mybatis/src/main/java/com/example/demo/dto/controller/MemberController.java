d7092496082cfcc4dd534702276a98f53f301f8b
GeonHwiAhn <169108642+GeonHwiAhn@users.noreply.github.com>
2024-06-17 14:02:09 +0900

    maven

--- demoProject3-mybatis/src/main/java/com/example/demo/dto/controller/MemberController.java
+++ demoProject3-mybatis/src/main/java/com/example/demo/dto/controller/MemberController.java
@@ -15,8 +15,12 @@ public class MemberController {
 	private MemberService memberService;
 	
 	//회원가입을 위해 작성할 GetMapping 작성하기
+	//회원가입으로 이동해서 작성하길 원한다면 /register 라는 주소명을 작성해주고
+	//홈페이지에서 아무것도 작상안한 맨 처음부터 회원가입을 보길 원한다면 "/" 작성
+	
 	@GetMapping("/register")
-	public String 회원가입폼보기(Model model) {
+	//메서드명은 한글이 아니라 영어로 작성
+	public String registerForm(Model model) {
 		model.addAttribute("mem", new Member());
 		return "index";
 	}
@@ -24,7 +28,8 @@ public class MemberController {
 	@PostMapping("/register")
 	public String 회원가입완료(Member member, Model model) {
 		//회원가입 작성이 완료가 되면 db에 저장하겠다.
-		memberService.멤버가입(member);
+		//마찬가지로 멤버가입으로 작성한 메서드기능 호출명을 insertMember로 변경해줌
+		memberService.insertMember(member);
 		//model.addAttribute("select로 db에 저장된 회원가입정보 가져오기")
 		model.addAttribute("msg", "멤버가 성공적으로 가입됐습니다.");
 		return"success";
