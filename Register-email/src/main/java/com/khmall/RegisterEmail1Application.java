74a5a9cba3e26094053dcd97d599ad13c8068e0f
GeonHwiAhn <169108642+GeonHwiAhn@users.noreply.github.com>
2024-06-27 11:21:18 +0900

    email emailHelper

---
+++ Register-email/src/main/java/com/khmall/RegisterEmail1Application.java
@@ -0,0 +1,13 @@
+package com.khmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterEmail1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterEmail1Application.class, args);
+	}
+
+}
