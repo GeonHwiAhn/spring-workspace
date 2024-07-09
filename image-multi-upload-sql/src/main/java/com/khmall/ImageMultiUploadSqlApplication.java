51c4d37f808243f32ccd0a60b47a68437fa9fc33
GeonHwiAhn <169108642+GeonHwiAhn@users.noreply.github.com>
2024-07-09 15:16:57 +0900

    ajax

--- image-multi-upload-sql/src/main/java/com/khmall/ImageMultiUploadSqlApplication.java
+++ image-multi-upload-sql/src/main/java/com/khmall/ImageMultiUploadSqlApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RegisterEmailApplication {
+public class ImageMultiUploadSqlApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RegisterEmailApplication.class, args);
+		SpringApplication.run(ImageMultiUploadSqlApplication.class, args);
 	}
 
 }
---
+++ image-multi-upload-sql/src/main/java/com/khmall/dto/ImageUpload.java
@@ -0,0 +1,18 @@
+package com.khmall.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ImageUpload {
+	private int id;
+	private String file_name;
+	private String upload_dir;
+}
